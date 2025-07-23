75feff1e605444f1448dd23e912718d2dc2a97fb
merchani <ian.merchant@capgemini.com>
2025-07-23 12:52:31 +0100

    sorting filter for duedate
    add:
    - ascending dates sort
    - descending dates sort
    - specific duedates filter

--- src/DatabasePageController.java
+++ src/DatabasePageController.java
@@ -1,7 +1,13 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -120,6 +126,61 @@ public class DatabasePageController {
             }
         });
 
+        databasePage.getDueDateDropDownButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JPopupMenu bucketMenu = new JPopupMenu();
+                SQLHandler.getInstance().loadTasksFromDatabase();
+                Set<String> uniqueSet = new HashSet<String>();
+                for (int i = 0; i < tasks.size(); i++) {
+                    Task task = tasks.get(i);
+                    uniqueSet.add(task.getDueDate());
+                    System.out.print("This is a date: " + task.getDueDate()+"\n");
+                }
+                List<String> ascendingDates = sortDateStringsAscending(uniqueSet);
+                List<String> descendingDates = sortDateStringsDescending(uniqueSet);
+
+                for (String option : ascendingDates) {
+                    JMenuItem item = new JMenuItem(option);
+                    item.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            JOptionPane.showMessageDialog(databasePage.getDatabaseFrame(), "Selected: " + option);
+                            filterTasksByDueDate(option);
+                        }
+                    });
+                    bucketMenu.add(item);
+                }
+                JMenuItem ascendingItem = new JMenuItem("Ascending Order");
+                ascendingItem.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        filterSortedTaskList(descendingDates); 
+                    }
+                });
+                JMenuItem descendingItem = new JMenuItem("Descending Order");
+                descendingItem.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                        filterSortedTaskList(ascendingDates); 
+                    }
+                });
+                JMenuItem resetItem = new JMenuItem("Show All");
+                resetItem.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        resetTaskList(); 
+                    }
+                });
+                bucketMenu.addSeparator(); // Optional: separates reset from other options
+                bucketMenu.add(ascendingItem);
+                bucketMenu.add(descendingItem);
+                bucketMenu.add(resetItem);
+                bucketMenu.show(databasePage.getDueDateDropDownButton(), 0, databasePage.getDueDateDropDownButton().getHeight());
+            }
+        });
+
 
         databasePage.getPlannerButton().addActionListener(new ActionListener() {
             @Override
@@ -217,11 +278,86 @@ public class DatabasePageController {
         tasks.removeAllTasks();
     }
 
+    public void filterTasksByDueDate(String dueDate) {
+        List<Task> filtered = new ArrayList<>();
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            if (task.getDueDate().equals(dueDate)) {
+                filtered.add(task);
+            }
+        }
+        databasePage.clearTasksFromGrid();
+        for (Task task : filtered) {
+            databasePage.addExistingTask(task);
+        }
+        databasePage.highlightSearchMode();
+        tasks.removeAllTasks();
+    }
+
 
     public void resetTaskList() {
         databasePage.clearTasksFromGrid();
         loadExistingItemstoRows();
         databasePage.clearSearchHighlight();
     }
+
+    public void filterSortedTaskList(List<String> sortedDates) {
+        List<Task> filtered = new ArrayList<>();
+        for (String currentDate : sortedDates){
+            for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+                if (task.getDueDate().equals(currentDate)) {
+                    filtered.add(task);
+                }
+            }
+        }
+        databasePage.clearTasksFromGrid();
+        for (Task task : filtered) {
+            databasePage.addExistingTask(task);
+        }
+        databasePage.highlightSearchMode();
+        tasks.removeAllTasks();
+    }
+
+
+    public static List<String> sortDateStringsAscending(Set<String> dateStrings) {
+        List<String> sortedDates = new ArrayList<>(dateStrings);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Collections.sort(sortedDates, new Comparator<String>() {
+            @Override
+            public int compare(String d1, String d2) {
+                try {
+                    return sdf.parse(d1).compareTo(sdf.parse(d2));
+                } catch (ParseException e) {
+                    throw new IllegalArgumentException("Invalid date format", e);
+                }
+            }
+        });
+
+        return sortedDates;
+    }
+
+    public static List<String> sortDateStringsDescending(Set<String> dateStrings) {
+        List<String> sortedDates = new ArrayList<>(dateStrings);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Collections.sort(sortedDates, new Comparator<String>() {
+            @Override
+            public int compare(String d1, String d2) {
+                try {
+                    return sdf.parse(d2).compareTo(sdf.parse(d1));
+                } catch (ParseException e) {
+                    throw new IllegalArgumentException("Invalid date format", e);
+                }
+            }
+        });
+
+        return sortedDates;
+    }
+
+
+
+
 
 }
